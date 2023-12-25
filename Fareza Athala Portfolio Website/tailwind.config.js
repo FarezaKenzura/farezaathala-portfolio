3aacf8675e5b52eaade59911b9c1d2f92eed29f1
Fareza Athala <92410692+FarezaKenzura@users.noreply.github.com>
2023-12-25 20:28:52 +0700

    add Ver 0.1

---
+++ Fareza Athala Portfolio Website/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#0284c7',
+        secondary: '#64748b',
+        dark: '#020617',
+      },
+      screens: {
+        '2xl': '1320px',
+      }
+    },
+  },
+  plugins: [
+    require('tailwind-scrollbar')({ nocompatible: true }),
+  ],
+}
+
